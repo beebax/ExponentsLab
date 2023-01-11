3ea1f2a6a0cd28405b3a1f8173154ead44d0c810
belle <belle.baxley@gmail.com>
2023-01-11 16:24:31 -0500

    initial commit

---
+++ ExponentsLab/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Learn your integers!");
+string WannaRestart;
+
+do
+{   //GET USER NUMBER
+    Console.WriteLine("Enter an integer:");
+    int userNum = int.Parse(Console.ReadLine());
+
+    //IF VALID INPUT
+    if(userNum > 0 && userNum <= 1290)
+    {
+        Console.WriteLine("Number\t\tSquared\t\tCubed");
+
+        for (int i = 1; i <= userNum; i++)
+        {
+        Console.WriteLine($"{i}\t\t{i * i}\t\t{i * i * i}");
+        }
+    }
+
+    //IF INVALID INPUT
+    else
+    {
+        Console.WriteLine("Please provide a valid input.");
+    }
+
+    //OFFER TO REPEAT
+    Console.WriteLine("Press Y to repeat, any other key to end.");
+    WannaRestart = Console.ReadLine();
+}
+
+while (WannaRestart == "Y");
+
+Console.WriteLine("Thank you, goodbye.");
